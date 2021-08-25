f63acece49bdc370252f6a0f888ba644c8c3ed17
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2021-08-24 22:01:32 -0400

    commit tree d8aa4cfc800bea651ccd8b3916ffa54fca32697e

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 require "pry"
 
-def reverse_each_word(array)
-    array.split.collect do |word|
-      flipIt = word.reverse()
-    binding.pry
-    end
+def reverse_each_word(string)
+    splitString = string.split.collect do |word|
 
+      flipIt = word.reverse()
+
+    end
+    splitString.join(" ")
 end
